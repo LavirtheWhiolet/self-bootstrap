c0a38d548ae5036571654521c3e1b48232c6278a
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-04-15 17:49:34 +0600

    ...

--- samples/calculator/run_with_sample_input_data.sh
+++ samples/calculator/run_with_sample_input_data.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-mkdir -p generated
 ruby -EUTF-8:UTF-8 -I. peg_parser_generator.rb samples/calculator/grammar.peg > generated/calculator.rb &&
 ruby -EUTF-8:UTF-8 generated/calculator.rb samples/calculator/sample_input_data.txt
