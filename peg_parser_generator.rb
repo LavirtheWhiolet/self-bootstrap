1df6b9a7938f34aa839bf57f7c3a224455e45cf3
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-04-16 18:17:26 +0600

    ...

--- peg_parser_generator.rb
+++ peg_parser_generator.rb
@@ -267,7 +267,7 @@ class PEGParserGenerator
     } or
     # any char.
     try {
-      pany_char and
+      pany_char and debug {"Oops!"} and
       Code.new %(@yy_input.getc), true  # It will return either character or nil. That suits us.
     } or
     # end
