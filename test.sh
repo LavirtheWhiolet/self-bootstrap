457dff445192af2ccd5d383bf32051026ef01a64
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-04-19 17:15:24 +0600

    ...

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
 # TODO: Delete before merging.
+echo '-- peg_parser_generator.peg -> peg_parser_generator.rb --' &&
 ruby -EUTF-8:UTF-8 peg_parser_generator.rb peg_parser_generator.peg > generated/peg_parser_generator.rb &&
+echo '-- test_parser.peg -> test_parser.rb --' &&
 ruby -EUTF-8:UTF-8 generated/peg_parser_generator.rb test_parser.peg > generated/test_parser.rb &&
+echo '-- test_parser.rb --' &&
 ruby -EUTF-8:UTF-8 generated/test_parser.rb
