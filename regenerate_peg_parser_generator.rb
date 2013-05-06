9bfdc872bf123bc29826c04b4a07d900f95638d3
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-05-06 16:20:31 +0600

    ...

--- regenerate_peg_parser_generator.rb
+++ regenerate_peg_parser_generator.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 # encoding: UTF-8
+require 'fileutils'
 
 def make_line_endings_unix_style(file)
   File.binwrite(file,
@@ -10,13 +11,14 @@ def make_line_endings_unix_style(file)
 end
 
 begin
-  source_generator = "peg_parser_generator.rb"
+  source_generator = resultant_generator = "peg_parser_generator.rb"
   10.times do |iteration|
     new_generator = "generated/peg_parser_generator#{iteration}.rb"
     system %(ruby #{source_generator} peg_parser_generator.peg > #{new_generator}) or
       exit $?.exitstatus
     make_line_endings_unix_style(new_generator)
     if File.binread(new_generator) == File.binread(source_generator)
+      FileUtils.mv new_generator, resultant_generator
       break
     end
     source_generator = new_generator
