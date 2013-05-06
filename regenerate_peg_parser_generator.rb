eba228fcd01542cc3c3ed9f1728c95259c31b003
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-05-06 16:01:49 +0600

    ...

--- regenerate_peg_parser_generator.rb
+++ regenerate_peg_parser_generator.rb
@@ -3,5 +3,7 @@
 iteration = 0
 generator = "peg_parser_generator.rb"
 10.times do
-  
+  new_generator = "generated/peg_parser_generator#{iteration}.rb"
+  system %(ruby #{generator} peg_parser_generator.peg > #{new_generator}) or
+    exit $?.exitstatus
 end
