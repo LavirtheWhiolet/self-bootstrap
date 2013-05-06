0e074c15aad25d91276db79c7647e8407fd5cc0c
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-05-06 16:10:44 +0600

    ...

--- regenerate_peg_parser_generator.rb
+++ regenerate_peg_parser_generator.rb
@@ -1,10 +1,17 @@
 #!/usr/bin/ruby
 
-source_generator = "peg_parser_generator.rb"
-10.times do |iteration|
-  new_generator = "generated/peg_parser_generator#{iteration}.rb"
-  system %(ruby #{source_generator} peg_parser_generator.peg > #{new_generator}) or
-    exit $?.exitstatus
-  break if File.read(new_generator) == File.read(source_generator)
-  source_generator = new_generator
+def make_line_endings_unix_style(file)
+  content = File.read(file).gsub("\r\n", "\n").gsub("\r", "\n")
+end
+
+begin
+  source_generator = "peg_parser_generator.rb"
+  10.times do |iteration|
+    new_generator = "generated/peg_parser_generator#{iteration}.rb"
+    system %(ruby #{source_generator} peg_parser_generator.peg > #{new_generator}) or
+      exit $?.exitstatus
+    make_line_endings_unix_style(new_generator)
+    break if File.read(new_generator) == File.read(source_generator)
+    source_generator = new_generator
+  end
 end
