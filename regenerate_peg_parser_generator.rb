68bee1984a79a85aef301f198a88463dcba945eb
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-05-06 16:07:15 +0600

    ...

--- regenerate_peg_parser_generator.rb
+++ regenerate_peg_parser_generator.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/ruby
 
-generator = "peg_parser_generator.rb"
+source_generator = "peg_parser_generator.rb"
 10.times do |iteration|
   new_generator = "generated/peg_parser_generator#{iteration}.rb"
-  system %(ruby #{generator} peg_parser_generator.peg > #{new_generator}) or
+  system %(ruby #{source_generator} peg_parser_generator.peg > #{new_generator}) or
     exit $?.exitstatus
-  break if File.read(new_generator) == File.read(generator)
-  generator = new_generator
+  break if File.read(new_generator) == File.read(source_generator)
+  source_generator = new_generator
 end
