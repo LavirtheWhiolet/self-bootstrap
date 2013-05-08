e0c628a6b775e548cdab76eaf44daed21bc7f8f5
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-05-08 16:03:46 +0600

    ...

--- samples/parser_xml_s_kwadratny_skobkamy/run.sh
+++ samples/parser_xml_s_kwadratny_skobkamy/run.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+ruby peg_parser_generator.rb samples/parser_xml_s_kwadratny_skobkamy/grammar.peg > generated/parser.rb &&
+ruby generated/parser.rb samples/parser_xml_s_kwadratny_skobkamy/sample_input.txt
