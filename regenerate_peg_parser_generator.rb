c6d24eacb2baa27611cd8c550b61ab8db29917f6
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-05-14 17:18:11 +0600

    ...

--- regenerate_peg_parser_generator.rb
+++ regenerate_peg_parser_generator.rb
@@ -14,13 +14,13 @@ begin
   source_generator = resultant_generator = "peg_parser_generator.rb"
   10.times do |iteration|
     new_generator = "generated/peg_parser_generator#{iteration}.rb"
-    STDERR.puts %(Iteration #{iteration}...)
-    STDERR.puts %(Generating "#{new_generator}"...)
+    STDERR.puts %(Iteration #{iteration}: generating "#{new_generator}"...)
     system %(ruby #{source_generator} peg_parser_generator.peg > #{new_generator}) or
       exit $?.exitstatus
     make_line_endings_unix_style(new_generator)
     if File.binread(new_generator) == File.binread(source_generator)
       FileUtils.mv new_generator, resultant_generator
+      STDERR.puts %(Done.)
       break
     end
     source_generator = new_generator
