fa0f9da445869ccc95ae534611456b05d42fc4e8
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-05-17 17:12:00 +0600

    ...

--- samples/star_of_David/run.sh
+++ samples/star_of_David/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 ruby peg_parser_generator.rb samples/star_of_David/grammar.peg > generated/parser.rb &&
-ruby generated/parser.rb samples/star_of_David/data.txt
+ruby generated/parser.rb samples/star_of_David/data.txt &&
+echo "TEXT OBREZANY"
