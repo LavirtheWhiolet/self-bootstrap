14544f2408f94f7661a3370e80ac74c632b69881
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-06-26 18:58:38 +0600

    ...

--- samples/XML_with_brackets/run.sh
+++ samples/XML_with_brackets/run.sh
@@ -5,6 +5,6 @@ parser=generated/parser.rb
 
 ruby peg_parser_generator.rb ${dir}/grammar.peg > ${parser} &&
 (
-  ruby ${parser} ${dir}/sample_input.txt
-  ruby ${parser} ${dir}/bad_sample_input.txt
+  ruby ${parser} ${dir}/input.txt
+  ruby ${parser} ${dir}/bad_input.txt
 )
