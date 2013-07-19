1b26f4b48cc285929132296843ec604c59982291
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2013-07-19 18:31:39 +0600

    ...

--- samples/INI_to_JSON/run.sh
+++ samples/INI_to_JSON/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ruby peg_parser_generator.rb samples/INI_to_JSON/grammar.peg > generated/INI_to_JSON.rb
+ruby peg2rb.rb samples/INI_to_JSON/grammar.peg > generated/INI_to_JSON.rb
 ruby generated/INI_to_JSON.rb samples/INI_to_JSON/input_data.ini
